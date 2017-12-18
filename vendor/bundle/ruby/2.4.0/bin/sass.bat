27cb2fc1f323dda2aa3dc5dd2e1092daa6e8b1cb
Jordan Smith <jsmith@letterlogic.com>
2017-12-18 13:19:36 -0600

    Removed files

--- vendor/bundle/ruby/2.4.0/bin/sass.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"C:\tools\ruby24\bin\ruby.exe" "C:/Users/jsmith/Documents/GitHub/jtsmith24.github.io/vendor/bundle/ruby/2.4.0/bin/sass" %1 %2 %3 %4 %5 %6 %7 %8 %9
-GOTO :EOF
-:WinNT
-@"C:\tools\ruby24\bin\ruby.exe" "%~dpn0" %*
