9c18c0d1cfcfb931b2a58808bff6454fa0eb5367
jordansmithdev <jordansmithdev@gmail.com>
2023-04-06 00:19:27 -0500

    Added maxlength and uppercase to header row

--- src/components/SumTable.tsx
+++ src/components/SumTable.tsx
@@ -122,12 +122,13 @@ const SumTable = ({
             {headerData.map((headerValue, colIndex) => (
               <th key={colIndex} className="h-12">
                 <input
-                  className="w-full text-center text-black sm:text-sm"
+                  className="w-full text-center uppercase text-black sm:text-sm"
                   type="text"
                   value={headerValue}
                   onChange={e => handleHeaderChange(e, colIndex)}
                   onKeyDown={e => handleTableKeyDown(e, 0, colIndex, true)}
                   id={`header-${colIndex}-0`}
+                  maxLength={3}
                 />
               </th>
             ))}
