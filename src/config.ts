577a27b87f2bab0b5f2a8488fd4576137da8f204
jordansmithdev <jordansmithdev@gmail.com>
2023-12-21 14:18:06 -0600

    Added more social icons, adjusted spacing

--- src/config.ts
+++ src/config.ts
@@ -19,15 +19,21 @@ export const LOGO_IMAGE = {
 
 export const SOCIALS: SocialObjects = [
   {
-    name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: ` ${SITE.title} on Github`,
-    active: false,
-  },
-  {
     name: "LinkedIn",
     href: "https://www.linkedin.com/in/jordan-smith-nashdev/",
     linkTitle: `${SITE.title} on LinkedIn`,
+    active: true,
+  },
+  {
+    name: "Github",
+    href: "https://github.com/jtsmith24",
+    linkTitle: `Github`,
+    active: true,
+  },
+  {
+    name: "YouTube",
+    href: "https://www.youtube.com/@JordanSmithFilms",
+    linkTitle: `YouTube channel`,
     active: true,
   },
   {
@@ -35,17 +41,5 @@ export const SOCIALS: SocialObjects = [
     href: "mailto:jordansmithdev@gmail.com",
     linkTitle: `Send an email to ${SITE.title}`,
     active: true,
-  },
-  {
-    name: "Twitter",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on Twitter`,
-    active: false,
-  },
-  {
-    name: "YouTube",
-    href: "https://github.com/satnaing/astro-paper",
-    linkTitle: `${SITE.title} on YouTube`,
-    active: false,
   },
 ];
