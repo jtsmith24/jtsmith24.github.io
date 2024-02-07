67ed0488162498377c36ca46b3022cc2f06c82dc
jordansmithdev <jordansmithdev@gmail.com>
2024-02-06 18:47:12 -0600

    Added project detail page for projects in portfolio content collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://jordansmithdev.com",
@@ -16,6 +18,7 @@ export default defineConfig({
     }),
     react(),
     sitemap(),
+    mdx(),
   ],
   markdown: {
     remarkPlugins: [
